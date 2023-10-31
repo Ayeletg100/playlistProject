48c111d7260e273b21aca736521f61fe3258ad93
EfratElimelech2502 <efratelimelech@hilma.tech>
2023-10-31 18:10:30 +0200

    old project

--- DB.js
+++ DB.js
@@ -1,34 +1,38 @@
-class User{
-    static count= 1;
-
-    constructor(username, password ){
-        this.id=count;
-        this.username=username;
-        this.password=password;
-        this._playlists=[];
-
-        count++;
+class User {
+    static count = 1;
+    constructor(username, password) {
+        this.id = User.count;
+        this.username = username;
+        this.password = password;
+        this._playlists = [];
+        User.count++;
     }
+
+    
+}
+
+class Playlist {
+    static count = 1;
+    constructor(name) {
+        this.id = Playlist.count;
+        this.name = name;
+        this.playlistSongs = [];
+        Playlist.count++;
+    }
+
 
 }
 
-class Playlist{
-    static count=1;
-    constructor(name){
-        this.id= count;
-        this.name=name;
-        this.songs=[];
-
-        count++;
+class Song {
+    static count = 1;
+    constructor(name, artist) {
+        this.id = Song.count;
+        this.name = name;
+        this.artist = artist;
+        Song.count++;
     }
 }
 
-class Song{
-    static count=1;
-    constructor(name, artist){
-        this.id=count
-        this.name=name;
-        this.artist=artist;
-    }
-}
-
+const songs = []
+songs.push(new Song('Happy', 'pharell wiliams'));
+localStorage.setItem("songs", JSON.stringify(songs))
--- login.js
+++ login.js
@@ -7,9 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // if (localStorage.getItem('currUser')) {
     //     switchPage('app');
     // } else {
-        let temp = login;
-        let content = temp.content;
-        currPage.appendChild(content.cloneNode(true));
+    let temp = login;
+    let content = temp.content;
+    currPage.appendChild(content.cloneNode(true));
     // }
     document.getElementById("submit").addEventListener("click", checkUserExistence)
 })
@@ -18,14 +18,17 @@ document.addEventListener('DOMContentLoaded', () => {
 // localStorage.setItem("users" , JSON.stringify([user]));
 
 function checkUserExistence() {
+    //server
     const name = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const usersStr = localStorage.getItem("users");
     const usersArr = JSON.parse(usersStr);
     for (element of usersArr) {
         if (element.username === name && element.password === password) {
-           //change the current user to the one who logged in
-            localStorage.setItem('currUser' , JSON.stringify(element));
+            //change the current user to the one who logged in
+            localStorage.setItem('currUser', JSON.stringify(element));
+
+            //client
             switchPage(app);
             return;
         }
