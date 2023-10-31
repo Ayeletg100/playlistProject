3b454f3341c9795c5f651832c7042f9e7ca5548a
EfratElimelech2502 <efratelimelech@hilma.tech>
2023-10-31 18:31:55 +0200

    new web password

--- DB.js
+++ DB.js
@@ -8,31 +8,15 @@ class User {
         User.count++;
     }
 
-    
+
+// const songs = []
+// songs.push(new Song('Happy', 'pharell wiliams'));
+// localStorage.setItem("songs", JSON.stringify(songs))
+
+//server
+function addWebPassword(webName, usernameW, passwordW) {
+    const newWP = new webPassword(webName, usernameW, passwordW);
+    webPasswords.push(newWP);
+    return webPasswords;
 }
 
-class Playlist {
-    static count = 1;
-    constructor(name) {
-        this.id = Playlist.count;
-        this.name = name;
-        this.playlistSongs = [];
-        Playlist.count++;
-    }
-
-
-}
-
-class Song {
-    static count = 1;
-    constructor(name, artist) {
-        this.id = Song.count;
-        this.name = name;
-        this.artist = artist;
-        Song.count++;
-    }
-}
-
-const songs = []
-songs.push(new Song('Happy', 'pharell wiliams'));
-localStorage.setItem("songs", JSON.stringify(songs))
